11a9773de8fa5103239f6bb81563a60a9d56cab3
Arpit Aggarwal <arpitaggarwal0511@gmail.com>
2025-10-18 02:04:29 +0530

    controls for editor

---
+++ dynamicui/src/components/controls/Select.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Select({ id, label, value, onChange }) {
+  return (
+    <div className="control-row">
+      <label htmlFor={id} className="control-label">{label}</label>
+      <input
+        id={id}
+        type="color"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="color-input"
+      />
+    </div>
+  );
+}
