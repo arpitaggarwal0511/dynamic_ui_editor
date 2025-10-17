ae7fb384a64e320d32d824e7db3bc53bb076f6e5
Arpit Aggarwal <arpitaggarwal0511@gmail.com>
2025-10-18 02:05:08 +0530

    controls setup in EditorSidebar

---
+++ dynamicui/src/components/EditorSidebar.jsx
@@ -0,0 +1,4 @@
+import ColorPicker from "./controls/ColorPicker";
+import Slider from "./controls/Slider";
+import Select from "./controls/Select";
+import ButtonGroup from "./controls/ButtonGroup";
